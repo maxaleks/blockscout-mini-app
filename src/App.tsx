c9bbd3498399bc7ec8a05b93a422fa8ed0fedc34
Max Alekseenko <nnwen0609@gmail.com>
2024-10-10 12:21:00 +0300

    smooth loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import WebApp from '@twa-dev/sdk';
 import StartPage from './StartPage';
 import TransactionPage from './TransactionPage';
 import AddressPage from './AddressPage';
+import LoadingPage from './LoadingPage';
 
 import { API_ENDPOINT } from './constants';
 
-const App: React.FC = () => {
-  const [startRoute, setStartRoute] = useState<string | null>(null);
+const Content: React.FC = () => {
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
+  const [startParamUsed, setStartParamUsed] = useState(false);
 
   useEffect(() => {
     WebApp.ready();
@@ -17,9 +20,14 @@ const App: React.FC = () => {
     } catch (error) {}
 
     const handleStartParam = async (startParam: string) => {
+      if (startParamUsed) {
+        setIsLoading(false);
+        return;
+      }
+
       try {
         const userId = WebApp.initDataUnsafe.user?.id.toString();
-        const response = await fetch(`${ API_ENDPOINT }/info`, {
+        const response = await fetch(`${API_ENDPOINT}/info`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -31,29 +39,46 @@ const App: React.FC = () => {
         }
         const { hash, chainId } = await response.json();
         if (hash.length === 42) {
-          setStartRoute(`/address?chainId=${chainId}&hash=${hash}`);
+          navigate(`/address?chainId=${chainId}&hash=${hash}`);
         } else if (hash.length === 66) {
-          setStartRoute(`/transaction?chainId=${chainId}&hash=${hash}`);
+          navigate(`/transaction?chainId=${chainId}&hash=${hash}`);
+        } else {
+          navigate('/');
         }
       } catch (error) {
         console.error('Error processing start parameter:', error);
-        // Handle error (e.g., show an error message to the user)
+        navigate('/');
+      } finally {
+        setIsLoading(false);
+        setStartParamUsed(true);
       }
     };
 
     const startParam = WebApp.initDataUnsafe.start_param;
     if (startParam) {
       handleStartParam(startParam);
+    } else {
+      setIsLoading(false);
     }
-  }, []);
+  }, [navigate, startParamUsed]);
+
+  if (isLoading) {
+    return <LoadingPage />;
+  }
 
   return (
+    <Routes>
+      <Route path="/" element={<StartPage />} />
+      <Route path="/transaction" element={<TransactionPage />} />
+      <Route path="/address" element={<AddressPage />} />
+    </Routes>
+  );
+}
+
+const App: React.FC = () => {
+  return (
     <Router>
-      <Routes>
-        <Route path="/" element={startRoute ? <Navigate to={startRoute} /> : <StartPage />} />
-        <Route path="/transaction" element={<TransactionPage />} />
-        <Route path="/address" element={<AddressPage />} />
-      </Routes>
+      <Content />
     </Router>
   );
 }
